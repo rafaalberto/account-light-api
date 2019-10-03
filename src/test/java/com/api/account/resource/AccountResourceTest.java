2fb56621fb990dda6c363bcd24ba2395dd5b5b3a
Rafael Alberto <s2it_ralberto@uolinc.com>
2019-10-03 15:33:43 -0300

    fix resource test

--- src/test/java/com/api/account/resource/AccountResourceTest.java
+++ src/test/java/com/api/account/resource/AccountResourceTest.java
@@ -1,10 +1,14 @@
 package com.api.account.resource;
 
+import com.api.account.AccountDaoImpl;
 import com.api.account.config.RoutesApplication;
 import com.api.account.model.Account;
+import com.api.account.repository.AccountDao;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.undertow.Undertow;
+import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
@@ -14,6 +18,8 @@ import static com.api.account.constants.HttpConstants.APP_HOST;
 public class AccountResourceTest {
 
     private Account accountInDB;
+
+    private AccountDao accountDao = new AccountDaoImpl();
 
     @Before
     public void setUp() {
@@ -27,14 +33,22 @@ public class AccountResourceTest {
 
         MockitoAnnotations.initMocks(this);
         accountInDB = new Account(1L, "Rafael");
+
     }
 
     @Test
     public void create() {
-        RestAssured.given().port(8090).get("/accounts/1").then()
+        Account accountCreated = accountDao.insert(new Account("Rafael"));
+        RestAssured.given().port(8090).get("/accounts/" + accountCreated.getId()).then()
                 .contentType(ContentType.JSON)
                 .assertThat()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", Matchers.equalTo("Rafael"));
+    }
+
+    @After
+    public void deleteAll() {
+        accountDao.deleteAll();
     }
 
 }
