1f646b6a4f1d3e4ea120bfe1e035200700f32ad9
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-10-06 18:06:28 -0300

    refactoring

--- src/test/java/com/api/account/AccountApiApplicationTest.java
+++ src/test/java/com/api/account/AccountApiApplicationTest.java
@@ -5,23 +5,20 @@ import com.api.account.resource.AccountResourceTest;
 import com.api.account.resource.TransactionResourceTest;
 import com.api.account.service.impl.AccountServiceImplTest;
 import com.api.account.service.impl.TransactionServiceImplTest;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import static org.junit.runners.Suite.*;
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
+@SuiteClasses({
         AccountDaoImplTest.class,
         AccountServiceImplTest.class,
         TransactionServiceImplTest.class,
         AccountResourceTest.class,
         TransactionResourceTest.class
 })
+
 public class AccountApiApplicationTest {
-
-    @Test
-    public void contextLoads() {
-    }
-
 
 }
